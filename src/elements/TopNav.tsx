a5e04bb5aafa40795219cbf672554e550181584f
Pulkit Kathuria <kevincobain2000@gmail.com>
2023-04-06 10:22:18 +0900

    spell

--- src/elements/TopNav.tsx
+++ src/elements/TopNav.tsx
@@ -4,7 +4,7 @@ export default function TopNav() {
 
     return (
         <div className="navbar grid place-items-center bg-base-300">
-            <a className="btn btn-ghost normal-case text-xl font-bold">
+            <a className="btn btn-ghost normal-case text-xl font-bold" href='./'>
                 Japan Drivers License
                 <img className="pl-3" src="images/img-x-original-x-04-x-1項目02-1-2-1-信号機赤点滅.no.jpg" alt="" height="76" width="76" />
             </a>
